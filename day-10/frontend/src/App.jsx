01de963ec81e9e9586c4ec1c4e9a2385b045fd66
mishkatjamal <mishkatjamal13@gmail.com>
2026-07-03 20:20:04 +0530

    day-10 changes still

--- day-10/frontend/src/App.jsx
+++ day-10/frontend/src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
 
   const [allnotes, setallnotes] = useState([])
 const fetchNotes =async ()=>{
-  const notes = await axios.get("/notes")
+  const notes = await axios.get("https://backend-class-fbun.onrender.com/api/notes")
   setallnotes(notes.data.notes)  
 }
 
